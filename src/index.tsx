3faa6006f55cff60c9d2a04e24554fcf3c49fe8a
Hugues Stefanski <hugues.stefanski@gmail.com>
2018-05-11 20:54:23 +0200

    Updated TypeScript, types, awesome typescript loader

--- src/index.tsx
+++ src/index.tsx
@@ -4,6 +4,6 @@ import * as ReactDOM from 'react-dom';
 import { Hello } from './components/hello';
 
 ReactDOM.render(
-  <Hello compiler="TypeScript 2.4.2" framework="React" />,
+  <Hello compiler="TypeScript 2.8.3" framework="React 16.3.2" />,
   document.getElementById('app')
 );
